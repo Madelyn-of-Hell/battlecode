f9154d5b104eb684e88ec9b95d124beb1d0ca3a3
Madelyn Belmen <maddie_bl@protonmail.com>
2026-06-11 08:48:37 +1000

    Added + integrated helpers to make testing a less clustered and error-prone nightmare NOTE IN DESC PAWLEY
    OK sorry for yelling but i didn't want to make the title super long. Essentially what I wanna say is:
    
    yes, there's been a massive break in commits. That is NOT equivalent to a massive break in my working on this project. I'm rather inexperienced at git and tend to set the borerlines for a commit at major milestones rather than small task completions, which has lead to me putting off an actual commit for ages. The goal I was *working* towards was having all my comms tests implemented, which i thought was a relatively small affair. Turns out, it was Not. Instead, I'm going to start committing each individual Set of tests as I go. Note that this timeline still doesn't perfectly represent the order in which things have actually been implemented but it's the most logical method in my mind to sync up these commits.
    
    Hopefully i won't have to do another spiel like this again; possibly i didn't need to do this in the first place but I want my intentions to be understood.

--- test/examplefuncsplayer/CommunicationTest.java
+++ test/examplefuncsplayer/CommunicationTest.java
@@ -12,7 +12,25 @@ import scala.reflect.internal.Trees;
 
 public class CommunicationTest {
 
+
 	final byte key = 0b01011010;
+	final int shared_mask = 0b01011010010110100101101001011010;
+	final int demo_rat_id = 0b0010110101101011011101011110100000;
+
+	/// Tiny little helper function to mask bits to make the rest of the work easier
+	public int mask(int value, int places) {
+		int mask_32 = 0b11111111111111111111111111111111;
+		int shaped_mask = mask_32 >>> (32-places); // The differen
+		// ce between a signed and unsigned bitshift kept me here from hours and I want you to know that I Suffer For My Craft
+		return value & shaped_mask;
+	}
+	@Test //but u know i gotta test it anyways lol
+	public void TestMask() {
+		int unmasked_value = 0b11101011010101010101111001010011;
+		assertEquals(0b1001010011, mask(unmasked_value, 10));
+		assertEquals(0b1111001010011, mask(unmasked_value, 13));
+	}
+
 	@Test
 	public void TestByteMask() {
 		final int correct_answer = 0b01011010010110100101101001011010;
@@ -24,17 +42,17 @@ public class CommunicationTest {
 	@Test
 	public void TestEncrypt() {
 		final int unencrypted_message = 0b01100011101010110110110100011101;
-		final int correct_answer = 0b00111001111100010011011101000111;
+		final int correct_answer = unencrypted_message ^ shared_mask;
 
 		int result = Communication.encrypt(unencrypted_message,key);
 		assertEquals(correct_answer,result);
 	}
 	@Test
 	public void TestDecrypt() {
-		final int unencrypted_message = 0b01100011101010110110110100011101;
-		final int correct_answer = 0b00111001111100010011011101000111;
+		final int encrypted_message = 0b01100011101010110110110100011101;
+		final int correct_answer = encrypted_message ^ shared_mask;
 
-		int result = Communication.encrypt(unencrypted_message,key);
+		int result = Communication.encrypt(encrypted_message,key);
 		assertEquals(correct_answer,result);
 	}
 
